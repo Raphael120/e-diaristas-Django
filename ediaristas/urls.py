b9577a390bd78396b878824af2ee2153984fea61
Raphael da Silva Araújo <raphaelaraujotoc@outlook.com>
2022-09-09 22:10:52 -0300

    alterando o redirecionamento da página inicial

--- ediaristas/urls.py
+++ ediaristas/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('web/', include('web.urls')),
+    path('', RedirectView.as_view(url='web/listar_diaristas', permanent=True)),
     path('api/', include('api.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
